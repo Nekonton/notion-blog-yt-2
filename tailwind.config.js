f196cd041ad919f5a17c8a4667bdeba742b07f33
Nekonton <m220l216@stu.kyoto-seika.ac.jp>
2023-09-27 11:52:44 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'], // 未使用のCSSを削除するための設定
-  darkMode: false, // 'media' or 'class'
+  // darkMode: false, // 'media' or 'class'
   theme: {
     extend: {},
   },
